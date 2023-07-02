e59e0c4f76d6d2c0aae72ec44c8a8cd37d8af1e1
onur.sak <onur.sak@trendyol.com>
2023-07-02 11:35:19 +0300

    remove logs

--- themes/cactus/static/js/main.js
+++ themes/cactus/static/js/main.js
@@ -57,8 +57,6 @@ $(document).ready(function() {
     if (menu.length) {
       $(window).on("scroll", function() {
         var topDistance = menu.offset().top;
-        console.log("Top distance: " + topDistance)
-        console.log("Is visible: " + nav.is(":visible"))
 
         // hide only the navigation links on desktop
         if (!nav.is(":visible") && topDistance < 100) {
